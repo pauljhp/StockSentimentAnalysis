9af9696cc66b605ee6ef94cf1812b41a37afb4ef
pauljhp <paulpbmo@gmail.com>
2022-06-30 22:09:59 +0800

    fixed bugs

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ cd ..
 conda env create -f ./StockSentimentAnalysis/environment.yml
 conda activate sent_env
 mkdir -p ./finBERT/models/sentiment/pretrained/finbert-sentiment/
-curl https://prosus-public.s3-eu-west-1.amazonaws.com/finbert/finbert-sentiment/pytorch_model.bin > ./finBERT/models/sentiment/pretrained/finbert-sentiment/pytorch_model.bin
+# curl https://prosus-public.s3-eu-west-1.amazonaws.com/finbert/finbert-sentiment/pytorch_model.bin > ./finBERT/models/sentiment/pretrained/finbert-sentiment/pytorch_model.bin
 cp ./finBERT/config.json ./finBERT/models/sentiment/pretrained/finbert-sentiment/config.json
